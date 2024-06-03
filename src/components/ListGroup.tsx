385e43834bac5f479f7afb32be844eaf4ffad177
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-03 08:50:20 +0100

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
-  const items = ["Lagos", "Enugu", "Rivers", "Kano", "Imo", "Sokoto"];
+  let items = ["Lagos", "Enugu", "Rivers", "Kano", "Imo", "Sokoto"];
+ 
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  
 
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p> No item found</p> : null}
+      {items.length === 0 && <p> No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-gropu-item"
+            }
+            key={item}
+            onClick={()=> {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
